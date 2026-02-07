694275d9082a3a9d309516c79b8533e17d391056
Jacek Sikora <jack.sikor@gmail.com>
2026-02-07 09:03:40 +0100

    prepare update the API logic to handle actors and movies

--- api/main.py
+++ api/main.py
@@ -7,11 +7,12 @@ import sqlite3
 
 
 class Actor(BaseModel):
-    name: str
+    fullname: str
 
 class Movie(BaseModel):
     title: str
-    year: str
+    year: int
+    description: str
     actors: list[Actor] = []
 
 app = FastAPI()
@@ -26,14 +27,25 @@ def serve_react_app():
 def get_movies():
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
-    movies_rows = cursor.execute('SELECT ID, title, year FROM movies').fetchall()
+    movies_rows = cursor.execute('SELECT ID, title, year, description FROM movies').fetchall()
 
     output = []
     for movie_row in movies_rows:
         movie_id = movie_row[0]
-        actors_rows = cursor.execute('SELECT name FROM actors WHERE movie_id=?', (movie_id,)).fetchall()
-        actors = [{'name': a[0]} for a in actors_rows]
-        movie = {'id': movie_id, 'title': movie_row[1], 'year': movie_row[2], 'actors': actors}
+        actors_rows = cursor.execute('''
+        SELECT a.fullname FROM actors a 
+        JOIN actor_movie am ON a.id = am.actor_id 
+            WHERE am.movie_id = ?
+        ''', (movie_id,)).fetchall()
+
+        actors = [{'fullname': a[0]} for a in actors_rows]
+        movie = {
+            'id': movie_id,
+            'title': movie_row[1],
+            'year': movie_row[2],
+            'description': movie_row[3],
+            'actors': actors
+        }
         output.append(movie)
     return output
 
@@ -41,11 +53,25 @@ def get_movies():
 def get_single_movie(movie_id: int):
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
-    movie = cursor.execute("SELECT ID, title, year FROM movies WHERE id=?", (movie_id,)).fetchone()
-    if movie is not None:
-        actors_rows = cursor.execute('SELECT name FROM actors WHERE movie_id=?', (movie_id,)).fetchall()
-        actors = [{'name': a[0]} for a in actors_rows]
-        return {'id': movie[0], 'title': movie[1], 'year': movie[2], 'actors': actors}
+    movie_row = cursor.execute("SELECT id, title, year, description FROM movies WHERE id=?", (movie_id,)).fetchone()
+
+    if movie_row is not None:
+        actors_rows = cursor.execute('''
+                                     SELECT a.fullname
+                                     FROM actors a
+                                              JOIN actor_movie am ON a.id = am.actor_id
+                                     WHERE am.movie_id = ?
+                                     ''', (movie_id,)).fetchall()
+
+        actors = [{'fullname': a[0]} for a in actors_rows]
+
+        return {
+            'id': movie_row[0],
+            'title': movie_row[1],
+            'year': movie_row[2],
+            'description': movie_row[3],
+            'actors': actors
+        }
     else:
         return {'message': "Movie not found"}
 
@@ -54,43 +80,59 @@ def get_single_movie(movie_id: int):
 def add_movie(movie: Movie):
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
-    cursor.execute("INSERT INTO movies (title, year) VALUES (?, ?)", (movie.title, movie.year))
+    cursor.execute("INSERT INTO movies (title, year, description) VALUES (?, ?, ?)",
+                   (movie.title, movie.year, movie.description))
     movie_id = cursor.lastrowid
 
     for actor in movie.actors:
-        cursor.execute("INSERT INTO actors (name, movie_id) VALUES (?, ?)", (actor.name, movie_id))
+        cursor.execute("SELECT id FROM actors WHERE fullname = ?", (actor.fullname,))
+        res = cursor.fetchone()
+        if res:
+            actor_id = res[0]
+        else:
+            cursor.execute("INSERT INTO actors (fullname) VALUES (?)", (actor.fullname,))
+            actor_id = cursor.lastrowid
+
+        cursor.execute("INSERT INTO actor_movie (actor_id, movie_id) VALUES (?, ?)", (actor_id, movie_id))
 
     db.commit()
     return {"message": "Movie and actors added successfully", "id": movie_id}
+
 
 @app.put("/movies/{movie_id}")
 def update_movie(movie_id: int, movie: Movie):
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
+
     cursor.execute(
-        "UPDATE movies SET title = ?, year = ? WHERE id = ?",
-        (movie.title, movie.year, movie_id)
+        "UPDATE movies SET title = ?, year = ?, description = ? WHERE id = ?",
+        (movie.title, movie.year, movie.description, movie_id)
     )
 
-    cursor.execute("DELETE FROM actors WHERE movie_id = ?", (movie_id,))
+    cursor.execute("DELETE FROM actor_movie WHERE movie_id = ?", (movie_id,))
+
     for actor in movie.actors:
-        cursor.execute("INSERT INTO actors (name, movie_id) VALUES (?, ?)", (actor.name, movie_id))
+        cursor.execute("SELECT id FROM actors WHERE fullname = ?", (actor.fullname,))
+        res = cursor.fetchone()
+
+        if res:
+            actor_id = res[0]
+        else:
+            cursor.execute("INSERT INTO actors (fullname) VALUES (?)", (actor.fullname,))
+            actor_id = cursor.lastrowid
+
+        cursor.execute("INSERT INTO actor_movie (actor_id, movie_id) VALUES (?, ?)", (actor_id, movie_id))
 
     db.commit()
-    if cursor.rowcount == 0:
-        pass
-
     return {"message": f"Movie with id = {movie_id} updated successfully"}
 
 @app.delete("/movies/{movie_id}")
 def delete_movie(movie_id: int):
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
-    cursor.execute("DELETE FROM actors WHERE movie_id = ?", (movie_id,))
+    cursor.execute("DELETE FROM actor_movie WHERE movie_id = ?", (movie_id,))
     cursor.execute("DELETE FROM movies WHERE id = ?", (movie_id,))
     db.commit()
-    if cursor.rowcount == 0:
-        return {"message": f"Movie with id = {movie_id} not found"}
     return {"message": f"Movie with id = {movie_id} deleted successfully"}
 
 @app.delete("/movies")
